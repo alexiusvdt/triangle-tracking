f510e147cff42a11bc311d037c189b8b10dd89c2
Alex Johnson <alex.johnson293@gmail.com>
2022-12-06 11:14:14 -0800

    logic & testing
    Co-authored-by: Daniel Yakovlev <daniel.yakovlev@gmail.com>

---
+++ Palindrome.Tests/PalindromeTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Words;
+
+namespace Words.Tests
+{
+  [TestClass]
+  public class PalindromeTests
+  {
+    // [TestMethod]
+    // public void IsNotTriangle_True()
+    // {
+    //   Assert.AreEqual("That's not a valid triangle", Triangle.IsTriangle(3,5,10));
+    // }
+  }
+}
